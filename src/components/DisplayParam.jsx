9b81879dae78df618fb65285678ea2c171b13f5a
Aidan Castellanos <castellanosaidan@gmail.com>
2024-02-01 16:03:59 -0800

    Created Ability to differ between num and word.

--- src/components/DisplayParam.jsx
+++ src/components/DisplayParam.jsx
@@ -2,10 +2,16 @@ import { useParams } from "react-router-dom"
 
 const DisplayParam = (props) => {
     const {userInput} = useParams();
+    console.log(userInput)
+    console.log(isNaN(userInput));
 
 return(
     <div>
-        <h1>The number is : {userInput}</h1>
+        {
+            isNaN(userInput)?
+            <h1>The word is : {userInput}</h1> :
+            <h1>The number is : {userInput}</h1>
+        }
     </div>
     )
 }
